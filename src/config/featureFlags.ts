644a2f032fd77c73dfab4502cbe13b152e02647d
22120278 <nvhphuc364@gmail.com>
2025-12-07 20:36:26 +0700

    Update feature flag for image processing

--- src/config/featureFlags.ts
+++ src/config/featureFlags.ts
@@ -2,7 +2,7 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const ENABLE_PROCESS_IMAGE = process.env.ENABLE_PROCESS_IMAGE === "true";
+const ENABLE_PROCESS_IMAGE = process.env.ENABLE_PROCESS_IMAGE !== "false";
 
 export const shouldProcessImages = () => ENABLE_PROCESS_IMAGE;
 
